02c0017c3ab99d13ff1df29e94c01ce441715432
Patrick Aljord <patcito@gmail.com>
2011-07-11 04:01:45 -0500

    implemented a site wide read-only mode

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_filter :login_required, :except => [:new, :create, :index, :show, :tags, :unanswered, :related_questions, :tags_for_autocomplete, :retag, :retag_to]
+  before_filter :check_read_only, :only => [:create, :new]
   before_filter :admin_required, :only => [:move, :move_to]
   before_filter :moderator_required, :only => [:close]
   before_filter :check_permissions, :only => [:solve, :unsolve, :destroy]
