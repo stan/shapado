8265553632b8be6f704e7ff99432727737a35af5
Patrick Aljord <patcito@gmail.com>
2012-02-20 19:25:20 -0500

    when a user changes the domain of a group, redirect to reset domain page

--- app/models/group.rb
+++ app/models/group.rb
@@ -163,6 +163,11 @@ class Group
   before_create :set_default_theme
   after_create :create_default_tags
 
+  def reset_custom_domain!
+    self.domain = "#{self.slug}.#{AppConfig.domain}"
+    self.save
+  end
+
   # use for export script, slow
   def members(*fields)
     fields += [:user_id]
