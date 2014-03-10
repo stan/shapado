aefea1576a6057bf177cae5d83da3bde0a002f20
Siebrand Mazeland <s.mazeland@xs4all.nl>
2014-03-10 18:57:11 +0000

    Localisation updates from https://translatewiki.net.

---
+++ app/assets/javascripts/i18n/haw.js
@@ -0,0 +1,12 @@
+/** Messages for Hawaiian (Hawai`i)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Kolonahe
+ */
+
+var I18n = {
+	on_leave_page: "Hoʻokahi manawa kūlia wale nō",
+	loading: "Hoʻouka nei..."
+};
+
